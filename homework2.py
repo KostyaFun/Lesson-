4c627b7bffbb80ccfdd17588f64324b0b3edbe89
KostyaFun <para-pa-pam11@mail.ru>
2024-06-20 15:35:21 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,7 @@
+homework=12
+time=1.5
+course= "Python"
+time_for_one_task= time/homework
+print(time_for_one_task)
+print("Курс"":", course + ",", "всего задач"":",homework,",","затрачено часов",":",time,",","среднее время выполнения", time_for_one_task, "часа")
+print('Курс: '+course+',всего задач:'+str(homework)+', затрачено часов: '+str(time)+', среднее время выполнения '+str(time_for_one_task))
